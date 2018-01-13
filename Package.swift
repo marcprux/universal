6fb586cf49329e421bde64377762ff045ce67cb0
Marc Prud'hommeaux <marc@prux.org>
2018-01-13 14:31:22 -0500

    Travis update

--- Package.swift
+++ Package.swift
@@ -1,6 +1,13 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-    name: "Bric-à-brac"
+    name: "Bric-à-brac",
+    products: [
+        .library(name: "BricBrac", targets: ["BricBrac"]),
+        ],
+    targets: [
+        .target(name: "BricBrac"),
+        .testTarget(name: "BricBracTests", dependencies: ["BricBrac"]),
+        ]
 )
-

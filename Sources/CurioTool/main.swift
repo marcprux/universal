ca43da183b0e5532c7b470054734674ae578f59d
marcprux <marc@glimpse.io>
2021-06-12 12:20:18 -0400

    chore: checkpoint

--- Sources/CurioTool/main.swift
+++ Sources/CurioTool/main.swift
@@ -8,10 +8,6 @@
 
 // usage: cat /opt/src/glimpse/glimpse/Glean/Glean/glean-schema.json | /Users/mprudhom/Library/Developer/Xcode/DerivedData/Glimpse-akzmpxhsvpypdxbqqszohlbdbwzm/Build/Products/Debug/CurioTool | xcrun -sdk macosx swiftc -parse -
 
-// The pure swift process handling is woefully simplistic; there is no stderr or process exiting, so
-// for the time being we throw errors when there is a problem with the arguments
-// We could alternatively import Darwin, but we'd like to keep this 100% pure Swift
-
-//import Curio // NOTE: we should not import any Frameworks here, but instead rely on the target's source file list
+import Curio
 
 try Curio.runWithArguments(CommandLine.arguments)

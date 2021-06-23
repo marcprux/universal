b06fc162f93260c9b8f795ade20b045b834e45e4
marcprux <marc@glimpse.io>
2021-06-23 04:06:52 -0400

    ci: build command line tool

--- Package.swift
+++ Package.swift
@@ -8,12 +8,15 @@ let package = Package(
         .library(name: "Curio", targets: ["Curio"]),
         .executable(name: "CurioTool", targets: ["CurioTool"]), // SR-1954
         ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "0.4.3")),
+    ],
     targets: [
         .target(name: "BricBrac"),
         .testTarget(name: "BricBracTests", dependencies: ["BricBrac"], resources: [.copy("testdata/")]),
         .target(name: "Curio", dependencies: ["BricBrac"]),
         .testTarget(name: "CurioTests", dependencies: ["BricBrac", "Curio"], resources: [.copy("schemas/")]),
-        .target(name: "CurioTool", dependencies: ["Curio"]), // SR-1954
+        .target(name: "CurioTool", dependencies: ["Curio", .product(name: "ArgumentParser", package: "swift-argument-parser")]), // SR-1954
         ]
 )
 
--- Sources/CurioTool/main.swift
+++ Sources/CurioTool/main.swift
@@ -9,5 +9,6 @@
 // usage: cat /opt/src/glimpse/glimpse/Glean/Glean/glean-schema.json | /Users/mprudhom/Library/Developer/Xcode/DerivedData/Glimpse-akzmpxhsvpypdxbqqszohlbdbwzm/Build/Products/Debug/CurioTool | xcrun -sdk macosx swiftc -parse -
 
 import Curio
+import ArgumentParser
 
 try Curio.runWithArguments(CommandLine.arguments)
